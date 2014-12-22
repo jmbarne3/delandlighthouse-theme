fa745576b44f1c4e0aa7865f76762ae7c52f53fe
Jim Barnes <james.barnes@ucf.edu>
2014-12-22 11:22:01 -0500

    Updaing news page.

--- shortcodes.php
+++ shortcodes.php
@@ -37,6 +37,7 @@ if ( ! function_exists('sc_slideshow') ) {
 		);
 
 		$slides = get_posts($args);
+		$carousel_id = $slideshow . "-carousel";
 
 		ob_start();
 
@@ -44,17 +45,17 @@ if ( ! function_exists('sc_slideshow') ) {
 
 		<div class="row">
 			<div class="<?php echo $col_size; ?>">
-				<div id="<?php echo $slideshow; ?>" class="carousel slide" data-ride="carousel">
+				<div id="<?php echo $carousel_id; ?>" class="carousel slide" data-ride="carousel">
 					<ol class="carousel-indicators">
 					<?php foreach ($slides as $key=>$slide) { ?>
-						<li data-target="#<?php echo $slideshow; ?>" data-slide-to="<?php echo $key; ?>" <?php if ( $key == 0 ): ?> class="active" <?php endif; ?></li>
+						<li data-target="#<?php echo $carousel_id; ?>" data-slide-to="<?php echo $key; ?>" <?php if ( $key == 0 ): ?> class="active" <?php endif; ?></li>
 					<?php } ?>
 					</ol>
 					<div class="carousel-inner" role="listbox">
 					<?php foreach ($slides as $key=>$slide) { 
 							$slide_image = get_field('slide_image', $slide->ID)['sizes']['large'];
 							$alt_text = get_field('alt_text', $slide->ID);
-							$caption = get_field('caption_html'. $slide->ID);
+							$caption = get_field('caption_html', $slide->ID);
 					?>
 						<div class="item <?php if ( $key == 0 ): echo 'active'; endif; ?>">
 							<img src="<?php echo $slide_image; ?>" alt="<?php echo $alt_text; ?>">
@@ -65,15 +66,13 @@ if ( ! function_exists('sc_slideshow') ) {
 							<?php endif; ?>
 						</div>
 					<?php } ?>
-					<a class="left carousel-control" href="#<?php echo $slideshow; ?>" role="button" data-slide="prev">
-						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-					    <span class="sr-only">Previous</span>
-					</a>
-					<a class="right carousel-control" href="#<?php echo $slideshow; ?>" role="button" data-slide="next">
-						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-					    <span class="sr-only">Previous</span>
-					</a>
 					</div>
+					<a class="left carousel-control" href="#<?php echo $carousel_id; ?>" role="button" data-slide="prev">
+						&lsaquo;
+					</a>
+					<a class="right carousel-control" href="#<?php echo $carousel_id; ?>" role="button" data-slide="next">
+						&rsaquo;
+					</a>
 				</div>
 			</div>
 		</div>
