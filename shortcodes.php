f443ba19b5f48526806a161ea003a41d03057fa0
Jim Barnes <jimbarnesdeveloper@gmail.com>
2015-01-14 23:07:07 -0500

    Updated orderby statement.

--- shortcodes.php
+++ shortcodes.php
@@ -31,7 +31,7 @@ if ( ! function_exists('sc_slideshow') ) {
 				'field' => 'slug',
 				'terms' => $slideshow,
 			),
-			'orderby' => 'order',
+			'orderby' => 'menu_order',
 			'order' => 'ASC',
 			'meta_query' => array( 
 				array( 
